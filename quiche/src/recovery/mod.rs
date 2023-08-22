a7e7dc1acff604ea069f596dbd844842e98c6fcf
Gina Yuan <gina.yuan@gmail.com>
2023-08-22 19:45:55 +0000

    higher default near subpath ratios

--- quiche/src/recovery/mod.rs
+++ quiche/src/recovery/mod.rs
@@ -95,7 +95,7 @@ pub(super) const MAX_OUTSTANDING_NON_ACK_ELICITING: usize = 24;
 
 // Client-side retransmission
 #[cfg(not(feature = "ack_reduction"))]
-const DEFAULT_NEAR_SUBPATH_RATIO: f64 = 2.0 / 152.0;
+const DEFAULT_NEAR_SUBPATH_RATIO: f64 = 1.0 / 26.0;
 #[cfg(not(feature = "ack_reduction"))]
 const SIDECAR_MARK_ACKED: bool = false;
 #[cfg(not(feature = "ack_reduction"))]
@@ -103,7 +103,7 @@ const SIDECAR_RESET_THRESHOLD: Duration = Duration::from_millis(10);
 
 // ACK reduction
 #[cfg(feature = "ack_reduction")]
-const DEFAULT_NEAR_SUBPATH_RATIO: f64 = 40.0 / 41.0;
+const DEFAULT_NEAR_SUBPATH_RATIO: f64 = 25.0 / 26.0;
 #[cfg(feature = "ack_reduction")]
 const SIDECAR_MARK_ACKED: bool = true;
 #[cfg(feature = "ack_reduction")]
